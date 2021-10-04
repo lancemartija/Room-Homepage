a63e0e4f7e5e99638dc6a3d0f2243424fb709fcc
Lance Martija <jacobmartija03@gmail.com>
2021-10-04 16:28:47 +0800

    feat: open and close burger menu functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
+const navMobileContainer = document.querySelector('.nav__link-container')
+
 document.addEventListener('click', e => {
-  // code goes here...
+  const isHamburgerButton = e.target.matches('[data-hamburger-button]')
+  const isCloseButton = e.target.matches('[data-close-button]')
+
+  if (isHamburgerButton) navMobileContainer.classList.toggle('active')
+  if (isCloseButton) navMobileContainer.classList.toggle('active')
+
 })
